8990d679664f7c662d1c8ab2ddfc46c141fd977e
Sanaullah-Turab <sanaullahmalik2003@gmail.com>
2025-03-10 14:24:44 +0500

    add logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { FiUser } from "react-icons/fi";
 import { HiMenu } from "react-icons/hi";
+import logoImage from "../assets/codeconvo.png";
 
 const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -11,9 +12,16 @@ const Navbar = () => {
     <nav className="bg-white px-6 py-3 shadow font-poppins">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800">
-          Code <span className="text-[#122a6b] -ml-2 ">Convo</span>
-        </h1>
+        <div className="flex items-center">
+          <img
+            src={logoImage}
+            alt="Code Convo Logo"
+            className="h-8 sm:h-10 mr-2"
+          />
+          <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800">
+            Code <span className="text-[#122a6b] -ml-2 ">Convo</span>
+          </h1>
+        </div>
 
         {/* Right side content */}
         <div className="flex items-center space-x-4 sm:space-x-6">
@@ -41,14 +49,14 @@ const Navbar = () => {
               {dropdownOpen && (
                 <div className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-md border">
                   <Link
-                    to="/login"
+                    to="/"
                     className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                     onClick={() => setDropdownOpen(false)}
                   >
                     Login
                   </Link>
                   <Link
-                    to="/signup"
+                    to="/"
                     className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                     onClick={() => setDropdownOpen(false)}
                   >
