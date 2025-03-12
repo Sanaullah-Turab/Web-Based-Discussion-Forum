75489b0024203d4a03e62b1aabb4fa1f43e479aa
Soban7Akram <msobanakram7@gmail.com>
2025-03-12 23:22:02 +0500

    almost completed backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'forums', views.ForumViewSet, basename='forums')
+router.register(r'forum-memberships', views.ForumMembershipViewSet, basename='forum-memberships')
+router.register(r'messages', views.MessageViewSet, basename='messages')
+router.register(r'message-mentions', views.MessageMentionViewSet, basename='message-mentions')
 
 urlpatterns = [
     path('', include(router.urls)),
